b0e782f9407a891236a06ffca8e09750d51855c0
vumg <ab17100222@gmail.com>
2023-10-25 23:16:02 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,15 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+    <!-- Add href links here -->
+    <a href="frm_alumno.html">Go to frm_alumno</a>
+    <a href="frm_calif.html">Go to frm_calif</a>
 
-  <!--
-  This script places a badge on your repl's full-browser view back to your repl's cover
-  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
-  teal, blue, blurple, magenta, pink!
-  -->
-  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+    <!--
+    This script places a badge on your repl's full-browser view back to your repl's cover
+    page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+    teal, blue, blurple, magenta, pink!
+    -->
+    <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
   </body>
 </html>
