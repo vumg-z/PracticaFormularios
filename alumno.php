e6a0c6749c11a1556d840464a012616a800d2b0f
vumg-z <ab17100222@gmail.com>
2023-10-30 05:28:37 +0000

    changes

--- alumno.php
+++ alumno.php
@@ -2,32 +2,72 @@
 <html>
 <head>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <title>Resultados del Formulario</title>
+    <title>Información del Alumno</title>
 </head>
 <body class="bg-gray-300 flex items-center justify-center min-h-screen">
 
-<div class="bg-white p-8 rounded shadow-lg text-center">
-    <h1 class="text-2xl mb-4">Información del Alumno</h1>
+<div class="bg-white p-8 rounded shadow-lg">
+    <h1 class="text-2xl mb-4 text-center">Información del Alumno</h1>
 
-    <!-- Mostrar el nombre -->
-    <div class="mb-4">
-        <strong class="block text-sm font-medium text-gray-600">Nombre:</strong>
-        <span><?php echo htmlspecialchars($_POST['nombre']); ?></span>
+    <div class="flex -mx-2">
+        <!-- Columna 1 -->
+        <div class="w-1/2 px-2">
+            <!-- Mostrar el Registro -->
+            <div class="mb-4">
+                <strong class="block text-sm font-medium text-gray-600">Registro:</strong>
+                <span><?php echo htmlspecialchars($_POST['Registro']); ?></span>
+            </div>
+
+            <!-- Mostrar el Nombre y ApeP -->
+            <div class="mb-4">
+                <strong class="block text-sm font-medium text-gray-600">Nombre:</strong>
+                <span><?php echo htmlspecialchars($_POST['Nombre']); ?></span>
+            </div>
+            <div class="mb-4">
+                <strong class="block text-sm font-medium text-gray-600">Apellido Paterno:</strong>
+                <span><?php echo htmlspecialchars($_POST['ApeP']); ?></span>
+            </div>
+
+            <!-- Mostrar el Celular -->
+            <div class="mb-4">
+                <strong class="block text-sm font-medium text-gray-600">Celular:</strong>
+                <span><?php echo htmlspecialchars($_POST['Celular']); ?></span>
+            </div>
+
+            <!-- Mostrar el Domicilio -->
+            <div class="mb-4">
+                <strong class="block text-sm font-medium text-gray-600">Domicilio:</strong>
+                <span><?php echo htmlspecialchars($_POST['Domicilio']); ?></span>
+            </div>
+        </div>
+
+        <!-- Columna 2 -->
+        <div class="w-1/2 px-2">
+            <!-- Mostrar ApeM -->
+            <div class="mb-4">
+                <strong class="block text-sm font-medium text-gray-600">Apellido Materno:</strong>
+                <span><?php echo htmlspecialchars($_POST['ApeM']); ?></span>
+            </div>
+
+            <!-- Mostrar el Sexo -->
+            <div class="mb-4">
+                <strong class="block text-sm font-medium text-gray-600">Sexo:</strong>
+                <span><?php echo htmlspecialchars($_POST['Sexo']); ?></span>
+            </div>
+
+            <!-- Mostrar el Municipio y Carrera -->
+            <div class="mb-4">
+                <strong class="block text-sm font-medium text-gray-600">Municipio (Código):</strong>
+                <span><?php echo htmlspecialchars($_POST['Municipio']); ?></span>
+            </div>
+            <div class="mb-4">
+                <strong class="block text-sm font-medium text-gray-600">Carrera (Código):</strong>
+                <span><?php echo htmlspecialchars($_POST['Carrera']); ?></span>
+            </div>
+        </div>
     </div>
 
-    <!-- Mostrar el género -->
-    <div class="mb-4">
-        <strong class="block text-sm font-medium text-gray-600">Género:</strong>
-        <span><?php echo htmlspecialchars($_POST['genero']); ?></span>
-    </div>
-
-    <!-- Mostrar la fecha de nacimiento -->
-    <div class="mb-4">
-        <strong class="block text-sm font-medium text-gray-600">Fecha de Nacimiento:</strong>
-        <span><?php echo htmlspecialchars($_POST['fechaNacimiento']); ?></span>
-    </div>
-
-    <a href="frm_alumno.html" class="text-blue-600 hover:underline">Volver al formulario</a>
+    <a href="frm_alumno.html" class="text-blue-600 hover:underline block text-center mt-4">Volver al formulario</a>
 </div>
 
 </body>
--- calif.php
+++ calif.php
@@ -2,32 +2,58 @@
 <html>
 <head>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <title>CHINO 17100222</title>
+    <title>Información de Calificación</title>
 </head>
 <body class="bg-gray-300 flex items-center justify-center min-h-screen">
 
-<div class="bg-white p-8 rounded shadow-lg text-center">
-    <h1 class="text-2xl mb-4">Calificación Ingresada</h1>
+<div class="bg-white p-8 rounded shadow-lg">
+    <h1 class="text-2xl mb-4 text-center">Información de Calificación</h1>
 
-    <!-- Mostrar el nombre del estudiante -->
-    <div class="mb-4">
-        <strong class="block text-sm font-medium text-gray-600">Nombre del Estudiante:</strong>
-        <span><?php echo htmlspecialchars($_POST['nombreEstudiante']); ?></span>
+    <div class="flex -mx-2">
+        <!-- Columna 1 -->
+        <div class="w-1/2 px-2">
+            <!-- Mostrar PK_Calificacion -->
+            <div class="mb-4">
+                <strong class="block text-sm font-medium text-gray-600">ID Calificación:</strong>
+                <span><?php echo htmlspecialchars($_POST['PK_Calificacion']); ?></span>
+            </div>
+
+            <!-- Mostrar Calificación -->
+            <div class="mb-4">
+                <strong class="block text-sm font-medium text-gray-600">Calificación:</strong>
+                <span><?php echo htmlspecialchars($_POST['Calificacion']); ?></span>
+            </div>
+
+            <!-- Mostrar Periodo -->
+            <div class="mb-4">
+                <strong class="block text-sm font-medium text-gray-600">Periodo:</strong>
+                <span><?php echo htmlspecialchars($_POST['Periodo']); ?></span>
+            </div>
+
+            <!-- Mostrar Alumno -->
+            <div class="mb-4">
+                <strong class="block text-sm font-medium text-gray-600">ID Alumno:</strong>
+                <span><?php echo htmlspecialchars($_POST['Alumno']); ?></span>
+            </div>
+        </div>
+
+        <!-- Columna 2 -->
+        <div class="w-1/2 px-2">
+            <!-- Mostrar Profesor -->
+            <div class="mb-4">
+                <strong class="block text-sm font-medium text-gray-600">ID Profesor:</strong>
+                <span><?php echo htmlspecialchars($_POST['Profesor']); ?></span>
+            </div>
+
+            <!-- Mostrar Asignatura -->
+            <div class="mb-4">
+                <strong class="block text-sm font-medium text-gray-600">Asignatura:</strong>
+                <span><?php echo htmlspecialchars($_POST['Asignatura']); ?></span>
+            </div>
+        </div>
     </div>
 
-    <!-- Mostrar la calificación -->
-    <div class="mb-4">
-        <strong class="block text-sm font-medium text-gray-600">Calificación:</strong>
-        <span><?php echo htmlspecialchars($_POST['calificacion']); ?></span>
-    </div>
-
-    <!-- Mostrar la materia -->
-    <div class="mb-4">
-        <strong class="block text-sm font-medium text-gray-600">Materia:</strong>
-        <span><?php echo htmlspecialchars($_POST['materia']); ?></span>
-    </div>
-
-    <a href="frm_calif.html" class="text-blue-600 hover:underline">Volver al formulario</a>
+    <a href="frm_calif.html" class="text-blue-600 hover:underline block text-center mt-4">Volver al formulario</a>
 </div>
 
 </body>
